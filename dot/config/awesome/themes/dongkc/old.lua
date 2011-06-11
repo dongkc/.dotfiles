cfb7e02919f70b4692bf3744f1824b95f3b4c3e8
kai dong <dongkechang@gmail.com>
2011-06-11 19:01:11 +0800

    check in my extra profiles of softwares

---
+++ dot/config/awesome/themes/dongkc/old.lua
@@ -0,0 +1,56 @@
+---------------------------
+-- Default awesome theme --
+---------------------------
+
+theme = {}
+
+--theme.font          = "nomospace 8"
+
+theme.bg_normal     = "#1a1918"
+theme.bg_focus      = "#1a1918"
+theme.bg_urgent     = "#b23535"
+theme.bg_minimize   = "#1a1918"
+
+theme.fg_normal     = "#807b76"
+theme.fg_focus      = "#9acd32"
+theme.fg_urgent     = "#b2b2b2"
+theme.fg_minimize   = "#333231"
+
+theme.border_width  = "1"
+theme.border_normal = "#050505"
+theme.border_focus  = "#333231"
+theme.border_marked = "#a4a4a4"
+
+
+theme.font          = "sans 8"
+
+theme.bg_normal     = "#222222"
+theme.bg_focus      = "#535d6c"
+theme.bg_urgent     = "#ff0000"
+theme.bg_minimize   = "#444444"
+
+theme.fg_normal     = "#aaaaaa"
+theme.fg_focus      = "#ffffff"
+theme.fg_urgent     = "#ffffff"
+theme.fg_minimize   = "#ffffff"
+
+theme.border_width  = "1"
+theme.border_normal = "#000000"
+theme.border_focus  = "#535d6c"
+theme.border_marked = "#91231c"
+
+-- TAGLIST
+-- theme.taglist_squares_sel   = "/root/.config/awesome/themes/dongkc/taglist/squarefw.png"
+--theme.taglist_squares_unsel = "/root/.config/awesome/themes/dongkc/taglist/squarew.png"
+
+--theme.tasklist_floating_icon = "/root/.config/awesome/themes/dongkc/tasklist/floatingw.png"
+
+-- MENU
+--theme.menu_submenu_icon = "/root/.config/awesome/themes/dongkc/submenu.png"
+--theme.menu_height = "12"
+--theme.menu_width  = "100"
+
+-- WALLPAPER
+--theme.wallpaper_cmd = { "awsetbg /root/.config/awesome/background/bg.png" }
+
+return theme
